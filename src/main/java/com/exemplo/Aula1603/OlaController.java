3857990024d00baa5da59c158964813ef78cdc39
Larissa <cc25332@g.unicamp.br>
2026-03-16 16:18:00 -0300

    commit inicial

---
+++ src/main/java/com/exemplo/Aula1603/OlaController.java
@@ -0,0 +1,18 @@
+package com.exemplo.Aula1603;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/")
+public class OlaController {
+    //tela da raiz 
+    @RequestMapping(method = RequestMethod.GET)
+    public String ola(){
+        return "Alo mundo de TI...";
+    }
+
+    @GetMapping("/ola")
+    public String getOla(){
+        return "CARALHO";
+    }
+     
+}
